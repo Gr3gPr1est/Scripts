321444bac5921d069edb966688ad8edc36f4a218
Gergo Pap <53934464+Gr3gPr1est@users.noreply.github.com>
2020-02-01 14:16:06 +0100

    Update Auto_.py

--- Auto_.py
+++ Auto_.py
@@ -1,16 +1,18 @@
 import sys
 import os
 from sys import argv
-print '''
 
+print '''
 ############################
 #######	   Auto_.py    #####
 #######	 Greg Priest   #####
 ############################
 '''
+iplist = argv[1]
+print "[+] Wordlist: "+iplist
 print "[+] in progress!" 
 print "[+] Take RedBull and wait! ;]"
-iplist = argv[1]
+
 with open(iplist) as f:
 	for line in f:
 		for word in line.split():
